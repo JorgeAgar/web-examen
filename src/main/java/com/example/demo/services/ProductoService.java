07bc6c274a043ecadcf98b6b32d23d7599650f12
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-06-13 12:12:51 -0500

    Añadido servicio de get producto por referencia

--- src/main/java/com/example/demo/services/ProductoService.java
+++ src/main/java/com/example/demo/services/ProductoService.java
@@ -29,6 +29,15 @@ public class ProductoService {
     public Producto getProductoById(int id) {
         return productoRepository.getReferenceById(id);
     }
+    
+    public Producto getProductoByReference(String ref) {
+    	List<Producto> todos = getAllProductos();
+    	for(Producto p : todos) {
+    		if(p.getReferencia().equals(ref))
+    			return p;
+    	}
+    	return null;
+    }
 
     public void addProducto(ProductoDTO dto) {
         Producto producto = new Producto();
