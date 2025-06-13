ef866650f52182a3fb4cfe8b744f7462faf8795d
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-06-13 11:03:33 -0500

    Añadido CajeroService

---
+++ src/main/java/com/example/demo/services/CajeroService.java
@@ -0,0 +1,43 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dto.CajeroDTO;
+import com.example.demo.entities.Cajero;
+import com.example.demo.entities.Tienda;
+import com.example.demo.repositories.CajeroRepository;
+
+import lombok.NoArgsConstructor;
+
+@Service
+@NoArgsConstructor
+public class CajeroService {
+
+	@Autowired
+	private CajeroRepository cajeroRepository;
+	
+	@Autowired
+	private TiendaService tiendaService;
+	
+	public List<Cajero> getAllCajeros() {
+		return cajeroRepository.findAll();
+	}
+	
+	public Cajero getCajeroById(int id) {
+		return cajeroRepository.getReferenceById(id);
+	}
+	
+	public void addCajero(CajeroDTO cajeroDTO) {
+		Cajero cajero = new Cajero();
+		cajero.setDocumento(cajeroDTO.getDocumento());
+		cajero.setEmail(cajeroDTO.getEmail());
+		cajero.setNombre(cajeroDTO.getNombre());
+		cajero.setToken(cajeroDTO.getToken());
+		Tienda tienda = tiendaService.getTiendaById(cajeroDTO.getTiendaId());
+		cajero.setTienda(tienda);
+		cajeroRepository.save(cajero);
+	}
+}
