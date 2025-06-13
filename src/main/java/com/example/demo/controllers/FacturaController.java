8dd3b901a8fc24ba0120435181a867047349b2d6
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-06-13 12:00:37 -0500

    hecho primer tipo de factura

---
+++ src/main/java/com/example/demo/controllers/FacturaController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.FacturaDTO;
+import com.example.demo.services.FacturaService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/")
+public class FacturaController {
+	
+	@Autowired
+	FacturaService facturaService;
+
+	@GetMapping("/crear/{idTienda}")
+	public FacturaDTO procesarFactura(@PathVariable String idTienda, @RequestBody int idCliente) {
+		return facturaService.generarFactura(idCliente);
+	}
+}
